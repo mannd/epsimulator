d8685970f0b72c4d2b878cf5e0658c39ffdd796b
David Mann <mannd@epstudiossoftware.com>
2006-05-23 04:09:17 +0000

    another test
    git-svn-id: https://epsimulator.svn.sourceforge.net/svnroot/epsimulator/epsimulator/trunk@2 023b118a-4c14-0410-b830-a687a8169676

---
+++ trunk/src/epsimulator.cpp
@@ -0,0 +1,42 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by David Mann, MD   *
+ *   mannd@epstudiossoftware.com   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+
+#include "epsimulator.h"
+
+#include <qlabel.h>
+
+#include <kmainwindow.h>
+#include <klocale.h>
+
+epsimulator::epsimulator()
+    : KMainWindow( 0, "epsimulator" )
+{
+    // set the shell's ui resource file
+    setXMLFile("epsimulatorui.rc");
+
+    new QLabel( "Hello World", this, "hello label" );
+}
+
+epsimulator::~epsimulator()
+{
+}
+
+#include "epsimulator.moc"
