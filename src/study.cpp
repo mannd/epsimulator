71528f5042b6d022c1ab384e9a19d31be6572b28
David Mann <mannd@epstudiossoftware.com>
2006-09-13 02:36:12 +0000

    added study.cpp
    git-svn-id: https://epsimulator.svn.sourceforge.net/svnroot/epsimulator/epsimulator/trunk@70 023b118a-4c14-0410-b830-a687a8169676

---
+++ src/study.cpp
@@ -0,0 +1,64 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by EP Studios, Inc.                                *
+ *   mannd@epstudiossoftware.com                                           *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+
+#include "study.h"
+
+Study::Study() {
+    patient_ = new Patient;
+}
+
+void Study::setPatient(Patient* patient) {
+    patient_ = patient;
+}
+
+void Study::setDate(QDateTime& date) {
+    date_ = date;
+}
+
+
+void Study::setTime(QDateTime& time) {
+    time_ = time;
+}
+
+void Study::setNumber(QString& number) {
+    number_ = number;
+}
+
+Patient* Study::patient() const {
+    return patient_;
+}
+
+QDateTime Study::date() const {
+    return date_;
+}
+
+
+QDateTime Study::time() const {
+    return time_;
+}
+
+QString Study::number() const {
+    return number_;
+}
+
+Study::~Study() {
+    delete patient_;
+}
