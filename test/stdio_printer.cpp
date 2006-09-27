c9836c9e850696508eac2ef817d654317473603a
David Mann <mannd@epstudiossoftware.com>
2006-09-27 01:31:18 +0000

    started adding test suite
    git-svn-id: https://epsimulator.svn.sourceforge.net/svnroot/epsimulator/epsimulator/trunk@79 023b118a-4c14-0410-b830-a687a8169676

---
+++ test/stdio_printer.cpp
@@ -0,0 +1,42 @@
+/* Generated file, do not edit */
+
+#ifndef CXXTEST_RUNNING
+#define CXXTEST_RUNNING
+#endif
+
+#include <cxxtest/TestListener.h>
+#include <cxxtest/TestTracker.h>
+#include <cxxtest/TestRunner.h>
+#include <cxxtest/RealDescriptions.h>
+#include <cxxtest/StdioPrinter.h>
+
+int main() {
+ return CxxTest::StdioPrinter().run();
+}
+#include "MyTestSuite.h"
+
+static MyTestSuite suite_MyTestSuite;
+
+static CxxTest::List Tests_MyTestSuite = { 0, 0 };
+CxxTest::StaticSuiteDescription suiteDescription_MyTestSuite( "MyTestSuite.h", 23, "MyTestSuite", suite_MyTestSuite, Tests_MyTestSuite );
+
+static class TestDescription_MyTestSuite_testAddition : public CxxTest::RealTestDescription {
+public:
+ TestDescription_MyTestSuite_testAddition() : CxxTest::RealTestDescription( Tests_MyTestSuite, suiteDescription_MyTestSuite, 25, "testAddition" ) {}
+ void runTest() { suite_MyTestSuite.testAddition(); }
+} testDescription_MyTestSuite_testAddition;
+
+#include "navigatortest.h"
+
+static NavigatorTest suite_NavigatorTest;
+
+static CxxTest::List Tests_NavigatorTest = { 0, 0 };
+CxxTest::StaticSuiteDescription suiteDescription_NavigatorTest( "navigatortest.h", 23, "NavigatorTest", suite_NavigatorTest, Tests_NavigatorTest );
+
+static class TestDescription_NavigatorTest_testNavigator : public CxxTest::RealTestDescription {
+public:
+ TestDescription_NavigatorTest_testNavigator() : CxxTest::RealTestDescription( Tests_NavigatorTest, suiteDescription_NavigatorTest, 25, "testNavigator" ) {}
+ void runTest() { suite_NavigatorTest.testNavigator(); }
+} testDescription_NavigatorTest_testNavigator;
+
+#include <cxxtest/Root.cpp>
